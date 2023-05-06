3837374260e6d395d4a4a0c1bf6713ef33cc2c9a
kuzxnia <kacper.kuzniarski@gmail.com>
2023-05-06 15:03:05 +0200

    ex3

---
+++ exercise_3/src/main.rs
@@ -0,0 +1,85 @@
+struct Library {
+    books: Vec<Book>,
+}
+
+struct Book {
+    title: String,
+    year: u16,
+}
+
+impl Book {
+    // This is a constructor, used below.
+    fn new(title: &str, year: u16) -> Book {
+        Book {
+            title: String::from(title),
+            year,
+        }
+    }
+}
+
+impl std::fmt::Display for Book {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{} ({})", self.title, self.year)
+    }
+}
+
+
+// Implement the methods below. Update the `self` parameter to
+// indicate the method's required level of ownership over the object:
+//
+// - `&self` for shared read-only access,
+// - `&mut self` for unique and mutable access,
+// - `self` for unique access by value.
+impl Library {
+    fn new() -> Library {
+        Library { books: Vec::new() }
+    }
+
+    fn len(&self) -> usize {
+        self.books.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.books.is_empty()
+    }
+
+    fn add_book(&mut self, book: Book) {
+        self.books.push(book)
+    }
+
+    fn print_books(&self) {
+        for book in &self.books {
+            println!("{}, published in {}", book.title, book.year);
+        }
+    }
+
+    fn oldest_book(&self) -> Option<&Book> {
+        self.books.iter().max_by_key(|book| book.year)
+    }
+}
+
+
+// This shows the desired behavior. Uncomment the code below and
+// implement the missing methods. You will need to update the
+// method signatures, including the "self" parameter! You may
+// also need to update the variable bindings within main.
+fn main() {
+    let mut library = Library::new();
+
+    println!("The library is empty: {}", library.is_empty());
+
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+
+    println!("The library is no longer empty: {}", library.is_empty());
+
+    library.print_books();
+
+    match library.oldest_book() {
+        None => println!("The library is empty!"),
+        Some(book) => println!("The oldest book is {book}"),
+    }
+
+    println!("The library has {} books", library.len());
+    library.print_books();
+}
